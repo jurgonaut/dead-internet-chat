f53a84d76a5dc7fc13df7f15c22ae42c52491c7c
jurij srebrnic <jurij.srebrnic@gmail.com>
2024-11-24 12:48:09 +0100

    Formatted file

--- services/flask/entrypoint.sh
+++ services/flask/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-DEPLOY_ENV=${DEPLOY_ENV:-"prod"}
+DEPLOY_ENV=${DEPLOY_ENV:-}
 LOG_LEVEL=${LOG_LEVEL:-"INFO"}
 RELOAD=""
 
@@ -10,13 +10,13 @@ cli_command=()
 if [ $DEPLOY_ENV = "debug" ]; then
     export GEVENT_SUPPORT=True
     RELOAD="true"
-    cli_command+=( debugpy --wait-for-client --listen 0.0.0.0:5678 -m )
+    cli_command+=(debugpy --wait-for-client --listen 0.0.0.0:5678 -m)
 fi
 
-cli_command+=( gunicorn --bind 0.0.0.0:5000 --log-level=${LOG_LEVEL} --chdir src wsgi:app -k gevent )
+cli_command+=(gunicorn --bind 0.0.0.0:5000 --log-level="${LOG_LEVEL}" --chdir src wsgi:app -k gevent)
 
 if [ $RELOAD = "true" ]; then
-    cli_command+=( --reload )
+    cli_command+=(--reload)
 fi
 
 echo "${cli_command[@]}"
